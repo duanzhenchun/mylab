42fdfadc1c50d02f5407afcc39f8895835b61ce4
whille <Whille_Wang@cn.dlink.com>
2013-08-19 12:46:30 +0800

    update

--- my_mlclass/ex3_nn.py
+++ my_mlclass/ex3_nn.py
@@ -24,43 +24,46 @@ def test(X, y, func, *args):
         print prd, y[i]
     print 'accuracy: %.2f' % (1.0 * nright / ntest)
 
-def forward(A0, theta0, theta1):
+def nnforward(A0, theta0, theta1):
     A1 = sigmoid(A0 * theta0.T)  # 5000 * 26 
     A2 = sigmoid(A1 * theta1.T)  # 5000 * 10,  h = A2
     return A1, A2
 
-def NNpredict(x, theta0, theta1):
-    A1, A2 = forward(x, theta0, theta1)
+def nnpredict(x, theta0, theta1):
+    A1, A2 = nnforward(x, theta0, theta1)
     return np.argmax(A2)
 
-def nerualnetwork(X, y, Steps=400, Lambda=0.01):
+def neuralnetwork(X, y, Steps=400, Lambda=0.01):
     # Layers = 3
+    m, n = X.shape
+    s0 = n
     s1 = 25 + 1
     s2 = np.unique(y).size
-    m, n = X.shape
+    
     # transpose y to matrix
     Y = np.zeros((y.size, s2))  # 5000 * 10
     for i in xrange(y.size):
         Y[i, y[i][0] - 1] = 1.0 
     
     epsilon = 6 ** .5 / (n ** .5 + s2 ** .5)
-    theta0 = rand_Eps(s1, X.shape[1], epsilon)  # 26 * 401
+    theta0 = rand_Eps(s1, s0, epsilon)  # 26 * 401
     theta1 = rand_Eps(s2, s1, epsilon)  # 10 *  26    
         
     A0 = X  # 5000,401
     
     for _ in xrange(Steps):
-        A1, A2 = forward(A0, theta0, theta1)
+        A1, A2 = nnforward(A0, theta0, theta1)
         delta2 = A2 - Y  #  5000 * 10  
         delta1 = np.multiply(delta2 * theta1, np.multiply(A1, 1.0 - A1))  # 5000 * 26 
     
-        print abs(delta2).sum()
+        print _, abs(delta2).sum()
         
         D1 = (delta2.T * A1 + Lambda * theta1) / m  # save as theta1
         D0 = (delta1.T * A0 + Lambda * theta0) / m 
         theta1 -= D1  
         theta0 -= D0 
-    test(X, y, NNpredict, theta0, theta1)
+    test(X, y, nnpredict, theta0, theta1)
 
 # logistic_predict(X, y)
-nerualnetwork(X, y, 1000)
+# given 4000 steps, accuracy reached nearly 100%
+neuralnetwork(X, y, 4000)
