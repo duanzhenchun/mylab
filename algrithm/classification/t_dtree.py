ace7be1c193edbc729880adb87961de9d1fe4e63
whille <Whille_Wang@cn.dlink.com>
2013-08-28 00:21:57 +0800

    update

--- algrithm/classification/t_dtree.py
+++ algrithm/classification/t_dtree.py
@@ -5,16 +5,18 @@ import math
 from sklearn import tree
 from sklearn.ensemble import ExtraTreesRegressor
     
-def cocodata():
+def loaddata(fname, Ylabels):
     import xlrd
-    data = xlrd.open_workbook('../doc/Cocacola top 200 20130803.xlsx')
-    sh1 = data.sheets()[0]
+    data = xlrd.open_workbook(fname)
+    startrow=2
+    sh1 = data.sheets()[1]
     head = sh1.row_values(0)
-    start = 'Photo'
-    end = 'Co-branding'
+    start = 'Not Campaign'
+    end = 'Video'
+    missingrow=set()
     res = []
     for i in range(head.index(start), head.index(end) + 1):
-        res.append(sh1.col_values(i, start_rowx=1))
+        res.append(sh1.col_values(i, start_rowx=startrow))
     X = []
     for i in range(len(res[0])):
         X.append([None, ] * len(res))
@@ -24,30 +26,23 @@ def cocodata():
                 X[j][i] = 0
             else:
                 X[j][i] = int(res[i][j])
-        Y_score = sh1.col_values(head.index('Score'), start_rowx=1)
-    for i in range(len(Y_score)):
-        try:
-            Y_score[i] = float(Y_score[i])
-        except:
-            Y_score[i] = 0.0
-            
-    Y_gender = sh1.col_values(head.index('Male'), start_rowx=1)
-    for i in range(len(Y_gender)):
-        if not Y_gender[i]:
-            Y_gender[i] = 0.5
+    res = []
+    for i in range(head.index(Ylabels[0]), head.index(Ylabels[-1]) + 1):
+        res.append(sh1.col_values(i, start_rowx=startrow))
+    Y = []
+    for i in range(len(res[0])):
+        Y.append([None, ] * len(res))
+    for i in range(len(res)):
+        for j in range(len(res[i])):
+            if res[i][j] == '':
+                missingrow.add(j)
+            else:
+                Y[j][i] = float(res[i][j])
+    for mis in sorted(missingrow,reverse=True):
+        X.pop(mis)
+        Y.pop(mis)
+    return X, Y
 
-    Y_sen = sh1.col_values(head.index('Sentiment'), start_rowx=1)
-    for i in range(len(Y_sen)):
-        if not Y_sen[i]:
-            Y_sen[i] = 0
-        elif Y_sen[i] != 0:
-            Y_sen[i] = 1
-    Y_level = sh1.col_values(head.index(u'层级'), start_rowx=1)
-    for i in range(len(Y_level)):
-        if not Y_level[i]:
-            Y_level[i] = 0
-    Y_retweets = sh1.col_values(head.index(u'转发数'), start_rowx=1)
-    return X, (Y_score, Y_gender, Y_sen, Y_level, Y_retweets)
 
 def dtree(X, Y):
     maxY = max(Y)
@@ -98,8 +93,11 @@ def sample_rotation(N, ratio=.2):
         yield multirange(), xrange(*loc)
         
 if __name__ == '__main__':
-    X, Ys = cocodata()
-    Ylables = 'Score', 'Male', 'Sentiment', 'retweet level', 'retweet num'
-    for i in range(len(Ys)):
-        print Ylables[i], '=' * 10
-        dtree(X, Ys[i])
+    fname = '../doc/for spss 2.xlsx'
+    Ylabels = u'Score    Retweet    Comment    Degree    男女比例差绝对值'.split()
+    
+    X, Y = loaddata(fname, Ylabels)
+    Y=zip(*Y)
+    for i in range(len(Y)):
+        print Ylabels[i], '=' * 10
+        dtree(X, Y[i])
