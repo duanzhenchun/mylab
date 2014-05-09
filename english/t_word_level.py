4b3927d9ad7968bc46dc19b9c01e241e5180d3e6
whille <Whille_Wang@cn.dlink.com>
2014-05-09 18:20:28 +0800

    update

--- english/t_word_level.py
+++ english/t_word_level.py
@@ -1,34 +1,56 @@
+#coding=utf8
 from nltk.corpus import wordnet
 import re
 from stemming.porter2 import stem
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
+import marshal
+import logging
+import tempfile
+import sys
+import os
+import random
 
-st = PorterStemmer()
-wnl = WordNetLemmatizer()
+
+log_console = logging.StreamHandler(sys.stderr)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(log_console)
+
+WDict=None
+Dic_uk=None
+initialized=False
+Word_pat = re.compile(r"[\w’']+|\W+")
+ST = PorterStemmer()
+Wnl= WordNetLemmatizer()
 
 def word_lem(w):
-    return st.stem(wnl.lemmatize(w.lower()))
+    w = w.lower()
+    if w in Dic_uk:
+        w=Dic_uk[w]
+    return ST.stem(Wnl.lemmatize(w))
 
-f=open('all.num')
-lst=f.readlines()
-dic={}
-for l in lst[-1:0:-1]:
-    t = l.strip().split(' ')
-    w = word_lem(t[1])
-    dic[w] = max(dic.get(w,0), int(t[0]))
+def init_dict(fname='all.num'):
+    f=open(fname)
+    lst=f.readlines()
+    dic={}
+    for l in lst[-1:0:-1]:
+        t = l.strip().split(' ')
+        w = word_lem(t[1])
+        dic[w] = max(dic.get(w,0), int(t[0]))
+    return dic
 
-txt="""She woke with a gasp, not knowing who she was, or where.
-The smell of blood was heavy in her nostrils… or was that her nightmare, lingering? She had dreamed of wolves again, of running through some dark pine forest with a great pack at her hells, hard on the scent of prey.
 
-Half-light filled the room, grey and gloomy. Shivering, she sat up in bed and ran a hand across her scalp. Stubble bristled against her palm. I need to shave before Izembaro sees. Mercy, I’m Mercy, and tonight I’ll be raped and murdered. Her true name was Mercedene, but Mercy was all anyone ever called her…
+def uk_us():
+    #http://www.tysto.com/uk-us-spelling-list.html
+    uk_f = open('uk.txt')
+    us_f = open('us.txt')
+    dic={}
+    for uk, us in zip(uk_f, us_f):
+        dic[uk.strip()]=us.strip()
+    return dic
 
-Except in dreams. She took a breath to quiet the howling in her heart, trying to remember more of what she’d dreamt, but most of it had gone already. There had been blood in it, though, and a full moon overhead, and a tree that watched her as she ran."""
-
-k=dic.get(word_lem('fastened'))
-Word_pat = re.compile(r"[\w']+|\W+")
-
-def gen_paras(txt):
+def gen_paras(dic, txt, k):
     for line in txt.split('\n'):
         yield '<p>'
         for w0 in Word_pat.findall(line):
@@ -71,7 +93,48 @@ def to_html(txt, title=""):
 </body>
 </html>""" %(title, txt)
 
-f=open('tmp.html', 'w')
-f.write(to_html(''.join(gen_paras(txt))))
-f.close()
 
+def init():
+    global WDict, Dic_uk, initialized
+    if initialized:
+        return
+    cache_file = os.path.join(tempfile.gettempdir(),"en_word_freq.cache")
+    load_fail = True 
+    if os.path.exists(cache_file):
+        logger.debug("loading model from cache %s" % cache_file)
+        try:
+            WDict, Dic_uk = marshal.load(open(cache_file,'rb'))
+            logger.debug('english dict len: %d' %len(WDict))
+            load_fail = False
+        except Exception, e:
+            print e
+    if load_fail:
+        Dic_uk = uk_us()
+        WDict = init_dict()
+        logger.debug("dumping model to file cache %s" % cache_file)
+        try:
+            tmp_suffix = "."+str(random.random())
+            with open(cache_file+tmp_suffix,'wb') as temp_cache_file:
+                marshal.dump((WDict, Dic_uk), temp_cache_file)
+            if os.name=='nt':
+                import shutil
+                replace_file = shutil.move
+            else:
+                replace_file = os.rename
+            replace_file(cache_file+tmp_suffix,cache_file)
+        except:
+            logger.error("dump cache file failed.")
+            logger.exception("")
+    initialized = True
+
+def test(fname):
+    print fname
+    txt=open(fname).read()
+    k=WDict.get(word_lem('freak'))
+    print 'k:%d' %k
+    f=open('%s.html' %fname, 'w')
+    f.write(to_html(''.join(gen_paras(WDict, txt, k))))
+    f.close()
+
+init()
+test('razors.edge.txt')
