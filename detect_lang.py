3dcc4e30f51eeb5f480763772e140ae1a7fbcedb
whille <whille@163.com>
2015-09-04 23:30:17 +0800

    update

--- detect_lang.py
+++ detect_lang.py
@@ -19,21 +19,21 @@ class NGram(object):
         self.table = {}
         self.parse_text(text)
         self.calculate_length()
- 
+
     def parse_text(self, text):
         chars = ' ' * self.n # initial sequence of spaces with length n
- 
+
         for letter in (" ".join(text.split()) + " "):
             chars = chars[1:] + letter # append letter to sequence of length n
             self.table[chars] = self.table.get(chars, 0) + 1 # increment count
- 
+
     def calculate_length(self):
         """ Treat the N-Gram table as a vector and return its scalar magnitude
         to be used for performing a vector-based search.
         """
         self.length = sum([x * x for x in self.table.values()]) ** 0.5
         return self.length
- 
+
     def __sub__(self, other):
         """ Find the difference between two NGram objects by finding the cosine
         of the angle between the two vector representations of the table of
@@ -42,16 +42,16 @@ class NGram(object):
         """
         if not isinstance(other, NGram):
             raise TypeError("Can't compare NGram with non-NGram object.")
- 
+
         if self.n != other.n:
             raise TypeError("Can't compare NGram objects of different size.")
- 
+
         total = 0
         for k in self.table:
             total += self.table[k] * other.table.get(k, 0)
- 
+
         return 1.0 - (float(total) )/ (float(self.length) * float(other.length))
- 
+
     def find_match(self, languages):
         """ Out of a list of NGrams that represent individual languages, return
         the best match.
