efdd0da61cb343b4730a240727d9e63653e77687
王志国 <whille@163.com>
2016-12-03 15:49:54 +0800

    update

--- t_domain/t_daemon.py
+++ t_domain/t_daemon.py
@@ -12,28 +12,29 @@ from logging.handlers import RotatingFileHandler
 logger = logging.getLogger("Rotating Log")
 logger.setLevel(logging.INFO)
 path = './this.log'
-handler = RotatingFileHandler(path, maxBytes=2000, backupCount=5)
+handler = RotatingFileHandler(path, maxBytes=1024**2, backupCount=5)
 logger.addHandler(handler)
+
 
 def loop():
     while True:
-        print 'Zzz...'
         logger.warn('Zxx...' * 10)
         logger.info('Zxx...' * 10)
         time.sleep(5)
 
 
-#  out = open('out.log', 'w+')
-here = os.path.dirname(os.path.abspath(__file__))
+def main():
+    here = os.path.dirname(os.path.abspath(__file__))
+
+    context = daemon.DaemonContext(working_directory=here,
+                                   pidfile=PIDLockFile('this.pid'),
+                                   files_preserve=[
+                                       handler.stream
+                                   ], )
+
+    with context:
+        loop()
 
 
-context = daemon.DaemonContext(
-    working_directory=here,
-    pidfile=PIDLockFile('this.pid'),
-     files_preserve = [
-         handler.stream
-     ],
-)
-
-with context:
-    loop()
+if __name__ == "__main__":
+    main()
